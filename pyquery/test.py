157d6c2f5be47b7a84fedad8733d4f5cbb1ef0ba
Guido Serra <zeph@fsfe.org>
2011-03-25 17:31:15 +0100

    fixed the test, due to a "space"

--- pyquery/test.py
+++ pyquery/test.py
@@ -397,13 +397,13 @@ class TestWebScrapping(unittest.TestCase):
     @with_net
     def test_get(self):
         d = pq('http://www.theonion.com/search/', {'q': 'inconsistency'}, method='get')
-        self.assertEqual(d('input [name=q]:last').val(), 'inconsistency')
+        self.assertEqual(d('input[name=q]:last').val(), 'inconsistency')
         self.assertEqual(d('.news-in-brief h3').text(), 'Slight Inconsistency Found In Bible')
     
     @with_net
     def test_post(self):
         d = pq('http://www.theonion.com/search/', {'q': 'inconsistency'}, method='post')
-        self.assertEqual(d('input [name=q]:last').val(), '') # the onion does not search on post
+        self.assertEqual(d('input[name=q]:last').val(), '') # the onion does not search on post
 
 if __name__ == '__main__':
     fails, total = unittest.main()
