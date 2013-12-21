d22159bb32510e9eacf6c5c2408a79792e99fe76
Gael Pasgrimaud <gael@gawel.org>
2013-12-21 15:39:24 +0100

    test pseudo classes; documentation effort

--- pyquery/cssselectpatch.py
+++ pyquery/cssselectpatch.py
@@ -49,62 +49,133 @@ class JQueryTranslator(cssselect_xpath.HTMLTranslator):
     xpathexpr_cls = XPathExpr
 
     def xpath_first_pseudo(self, xpath):
-        """Matches the first selected element.
+        """Matches the first selected element::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><p class="first"></p><p></p></div>')
+            >>> d('p:first')
+            [<p.first>]
+
+        ..
         """
         xpath.add_post_condition('position() = 1')
         return xpath
 
     def xpath_last_pseudo(self, xpath):
-        """Matches the last selected element.
+        """Matches the last selected element::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><p></p><p class="last"></p></div>')
+            >>> d('p:last')
+            [<p.last>]
+
+        ..
         """
         xpath.add_post_condition('position() = last()')
         return xpath
 
     def xpath_even_pseudo(self, xpath):
-        """Matches even elements, zero-indexed.
+        """Matches even elements, zero-indexed::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><p></p><p class="last"></p></div>')
+            >>> d('p:even')
+            [<p>]
+
+        ..
         """
         # the first element is 1 in xpath and 0 in python and js
         xpath.add_post_condition('position() mod 2 = 1')
         return xpath
 
     def xpath_odd_pseudo(self, xpath):
-        """Matches odd elements, zero-indexed.
+        """Matches odd elements, zero-indexed::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><p></p><p class="last"></p></div>')
+            >>> d('p:odd')
+            [<p.last>]
+
+        ..
         """
         xpath.add_post_condition('position() mod 2 = 0')
         return xpath
 
     def xpath_checked_pseudo(self, xpath):
-        """Matches odd elements, zero-indexed.
+        """Matches odd elements, zero-indexed::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input checked="checked"/></div>')
+            >>> d('input:checked')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@checked and name(.) = 'input'")
         return xpath
 
     def xpath_selected_pseudo(self, xpath):
-        """Matches all elements that are selected.
+        """Matches all elements that are selected::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<select><option selected="selected"/></select>')
+            >>> d('option:selected')
+            [<option>]
+
+        ..
         """
         xpath.add_condition("@selected and name(.) = 'option'")
         return xpath
 
     def xpath_disabled_pseudo(self, xpath):
-        """Matches all elements that are disabled.
+        """Matches all elements that are disabled::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input disabled="disabled"/></div>')
+            >>> d('input:disabled')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@disabled")
         return xpath
 
     def xpath_enabled_pseudo(self, xpath):
-        """Matches all elements that are enabled.
+        """Matches all elements that are enabled::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input value="foo" /></div>')
+            >>> d('input:enabled')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("not(@disabled) and name(.) = 'input'")
         return xpath
 
     def xpath_file_pseudo(self, xpath):
-        """Matches all input elements of type file.
+        """Matches all input elements of type file::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="file"/></div>')
+            >>> d('input:file')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@type = 'file' and name(.) = 'input'")
         return xpath
 
     def xpath_input_pseudo(self, xpath):
-        """Matches all input elements.
+        """Matches all input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery(('<div><input type="file"/>'
+            ...              '<textarea></textarea></div>'))
+            >>> d(':input')
+            [<input>, <textarea>]
+
+        ..
         """
         xpath.add_condition((
             "(name(.) = 'input' or name(.) = 'select') "
@@ -112,7 +183,15 @@ class JQueryTranslator(cssselect_xpath.HTMLTranslator):
         return xpath
 
     def xpath_button_pseudo(self, xpath):
-        """Matches all button input elements and the button element.
+        """Matches all button input elements and the button element::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery(('<div><input type="button"/>'
+            ...              '<button></button></div>'))
+            >>> d(':button')
+            [<input>, <button>]
+
+        ..
         """
         xpath.add_condition((
             "(@type = 'button' and name(.) = 'input') "
@@ -120,49 +199,118 @@ class JQueryTranslator(cssselect_xpath.HTMLTranslator):
         return xpath
 
     def xpath_radio_pseudo(self, xpath):
-        """Matches all radio input elements.
+        """Matches all radio input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="radio"/></div>')
+            >>> d('input:radio')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@type = 'radio' and name(.) = 'input'")
         return xpath
 
     def xpath_text_pseudo(self, xpath):
-        """Matches all text input elements.
+        """Matches all text input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="text"/></div>')
+            >>> d('input:text')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@type = 'text' and name(.) = 'input'")
         return xpath
 
     def xpath_checkbox_pseudo(self, xpath):
-        """Matches all checkbox input elements.
+        """Matches all checkbox input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="checkbox"/></div>')
+            >>> d('input:checkbox')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@type = 'checkbox' and name(.) = 'input'")
         return xpath
 
     def xpath_password_pseudo(self, xpath):
-        """Matches all password input elements.
+        """Matches all password input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="password"/></div>')
+            >>> d('input:password')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@type = 'password' and name(.) = 'input'")
         return xpath
 
     def xpath_submit_pseudo(self, xpath):
-        """Matches all submit input elements.
+        """Matches all submit input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="submit"/></div>')
+            >>> d('input:submit')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@type = 'submit' and name(.) = 'input'")
         return xpath
 
+    def xpath_hidden_pseudo(self, xpath):
+        """Matches all hidden input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="hidden"/></div>')
+            >>> d('input:hidden')
+            [<input>]
+
+        ..
+        """
+        xpath.add_condition("@type = 'hidden' and name(.) = 'input'")
+        return xpath
+
     def xpath_image_pseudo(self, xpath):
-        """Matches all image input elements.
+        """Matches all image input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="image"/></div>')
+            >>> d('input:image')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@type = 'image' and name(.) = 'input'")
         return xpath
 
     def xpath_reset_pseudo(self, xpath):
-        """Matches all reset input elements.
+        """Matches all reset input elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><input type="reset"/></div>')
+            >>> d('input:reset')
+            [<input>]
+
+        ..
         """
         xpath.add_condition("@type = 'reset' and name(.) = 'input'")
         return xpath
 
     def xpath_header_pseudo(self, xpath):
-        """Matches all header elelements (h1, ..., h6)
+        """Matches all header elelements (h1, ..., h6)::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><h1>title</h1></div>')
+            >>> d(':header')
+            [<h1>]
+
+        ..
         """
         # this seems kind of brute-force, is there a better way?
         xpath.add_condition((
@@ -171,19 +319,42 @@ class JQueryTranslator(cssselect_xpath.HTMLTranslator):
         return xpath
 
     def xpath_parent_pseudo(self, xpath):
-        """Match all elements that contain other elements
+        """Match all elements that contain other elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><h1><span>title</span></h1><h1/></div>')
+            >>> d('h1:parent')
+            [<h1>]
+
+        ..
         """
         xpath.add_condition("count(child::*) > 0")
         return xpath
 
     def xpath_empty_pseudo(self, xpath):
-        """Match all elements that do not contain other elements
+        """Match all elements that do not contain other elements::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><h1><span>title</span></h1><h2/></div>')
+            >>> d(':empty')
+            [<span>, <h2>]
+
+        ..
         """
         xpath.add_condition("count(child::*) = 0")
         return xpath
 
     def xpath_eq_function(self, xpath, function):
-        """Matches a single element by its index.
+        """Matches a single element by its index::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><h1 class="first"/><h1 class="last"/></div>')
+            >>> d('h1:eq(0)')
+            [<h1.first>]
+            >>> d('h1:eq(1)')
+            [<h1.last>]
+
+        ..
         """
         if function.argument_types() != ['NUMBER']:
             raise ExpressionError(
@@ -194,7 +365,14 @@ class JQueryTranslator(cssselect_xpath.HTMLTranslator):
         return xpath
 
     def xpath_gt_function(self, xpath, function):
-        """Matches all elements with an index over the given one.
+        """Matches all elements with an index over the given one::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><h1 class="first"/><h1 class="last"/></div>')
+            >>> d('h1:gt(0)')
+            [<h1.last>]
+
+        ..
         """
         if function.argument_types() != ['NUMBER']:
             raise ExpressionError(
@@ -205,7 +383,14 @@ class JQueryTranslator(cssselect_xpath.HTMLTranslator):
         return xpath
 
     def xpath_lt_function(self, xpath, function):
-        """Matches all elements with an index below the given one.
+        """Matches all elements with an index below the given one::
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><h1 class="first"/><h1 class="last"/></div>')
+            >>> d('h1:lt(1)')
+            [<h1.first>]
+
+        ..
         """
         if function.argument_types() != ['NUMBER']:
             raise ExpressionError(
@@ -218,6 +403,13 @@ class JQueryTranslator(cssselect_xpath.HTMLTranslator):
 
     def xpath_contains_function(self, xpath, function):
         """Matches all elements that contain the given text
+
+            >>> from pyquery import PyQuery
+            >>> d = PyQuery('<div><h1/><h1 class="title">title</h1></div>')
+            >>> d(':contains("title")')
+            [<h1.title>]
+
+        ..
         """
         if function.argument_types() != ['STRING']:
             raise ExpressionError(
