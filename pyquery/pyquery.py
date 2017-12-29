264bc9d0d193efa75faf54e9aba7238a016529d1
Dominik Drożak <dominik@drozak.net>
2017-12-29 15:56:31 +0100

    Update pyquery.py

--- pyquery/pyquery.py
+++ pyquery/pyquery.py
@@ -1073,7 +1073,7 @@ class PyQuery(list):
             tag = self[0]
             children = tag.getchildren()
             if not children:
-                return h_entity.encode(tag.text)
+                return h_entity.escape(tag.text)
             html = tag.text or ''
             if 'encoding' not in kwargs:
                 kwargs['encoding'] = text_type
