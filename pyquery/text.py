2e7b23ac24164347f0bc7443d9f6856e5f436356
Gael Pasgrimaud <gael@gawel.org>
2017-12-30 12:12:08 +0100

    .clear() compat; pep8

--- pyquery/text.py
+++ pyquery/text.py
@@ -18,7 +18,8 @@ WHITESPACE_RE = re.compile('[\x20\x09\x0C\u200B\x0A\x0D]+')
 
 
 def squash_html_whitespace(text):
-    # use raw extract_text for preformatted content (like <pre> content or set by CSS rules)
+    # use raw extract_text for preformatted content (like <pre> content or set
+    # by CSS rules)
     # apply this function on top of
     return WHITESPACE_RE.sub(' ', text)
 
@@ -39,9 +40,11 @@ def _strip_artifical_nl(parts):
     if not parts:
         return parts
     for start_idx, pt in enumerate(parts):
-        if isinstance(pt, str):  # 0, 1, 2, index of first string [start_idx:...
+        if isinstance(pt, str):
+            # 0, 1, 2, index of first string [start_idx:...
             break
-    for end_idx, pt in enumerate(parts[:start_idx - 1 if start_idx > 0 else None:-1]):
+    iterator = enumerate(parts[:start_idx - 1 if start_idx > 0 else None:-1])
+    for end_idx, pt in iterator:
         if isinstance(pt, str):  # 0=None, 1=-1, 2=-2, index of last string
             break
     return parts[start_idx:-end_idx if end_idx > 0 else None]
@@ -55,7 +58,7 @@ def _merge_original_parts(parts):
             item = squash_html_whitespace(''.join(orp_buf)).strip()
             if item:
                 output.append(item)
-            orp_buf.clear()
+            orp_buf[:] = []
 
     for x in parts:
         if not isinstance(x, str):
@@ -74,15 +77,18 @@ def extract_text_array(dom, squash_artifical_nl=True, strip_artifical_nl=True):
     if dom.tag in SEPARATORS:
         r.append(True)  # equivalent of '\n' used to designate separators
     elif dom.tag not in INLINE_TAGS:
-        r.append(None)  # equivalent of '\n' used to designate artifically inserted newlines
+        # equivalent of '\n' used to designate artifically inserted newlines
+        r.append(None)
     if dom.text is not None:
         r.append(dom.text)
     for child in dom.getchildren():
-        r.extend(extract_text_array(child, squash_artifical_nl=False, strip_artifical_nl=False))
+        r.extend(extract_text_array(child, squash_artifical_nl=False,
+                                    strip_artifical_nl=False))
         if child.tail is not None:
             r.append(child.tail)
     if dom.tag not in INLINE_TAGS and dom.tag not in SEPARATORS:
-        r.append(None)  # equivalent of '\n' used to designate artifically inserted newlines
+        # equivalent of '\n' used to designate artifically inserted newlines
+        r.append(None)
     if squash_artifical_nl:
         r = _squash_artifical_nl(r)
     if strip_artifical_nl:
