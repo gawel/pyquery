4d5ee22b0f0f8027c0d1168234fc1329a40a66a3
Gael Pasgrimaud <gael@gawel.org>
2026-07-27 20:58:12 +0200

    bump version

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,4 +1,4 @@
-3.0.0 (unreleased)
+2.1.0 (unreleased)
 ------------------
 
 - pseudo selector ``:has()`` no longer support strings as arguments
--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ News
 
 """.format(**read('README', 'CHANGES'))
 
-version = '2.0.3.dev0'
+version = '2.1.0.dev0'
 
 setup(name='pyquery',
       version=version,
